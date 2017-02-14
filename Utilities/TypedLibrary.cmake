8a5d3d844aeccabb92a8ac5a4c6f92047f08131d
Kelly Brock <all8up@gmail.com>
2017-02-14 17:54:51 -0600

    Squashed 'CMake/' changes from 8df00a2..6e67618
    6e67618 Bunch of changes. Made refcount and atomic interface only libraries. Fixed various build problems after the change. Corrected some minor bits and pieces.
    f9370a0 External setup for gmock and spdlog.
    a0a3d80 Made it so eastl is a compile option. Changed config filename to setup so autocomplete is a bit nicer.
    b0f160d Added link to core libraries and changed the way unit tests are handled. Need to decide how to deal with required dependencies at this time.
    
    git-subtree-dir: CMake
    git-subtree-split: 6e67618dfc6dc02da18d2bdc02ecf38bba846353

--- Utilities/TypedLibrary.cmake
+++ Utilities/TypedLibrary.cmake
@@ -1,7 +1,7 @@
 # Utilities for dealing with potentially typed libraries.
 
 function( GetLibraryType name type libName )
-	string(REGEX MATCH "(.*):(static|shared|both)" typeInfo ${libName} )
+	string(REGEX MATCH "(.*):(static|shared|interface|both)" typeInfo ${libName} )
 	if (typeInfo)
 		set(${name} ${CMAKE_MATCH_1} PARENT_SCOPE)
 		set(${type} ${CMAKE_MATCH_2} PARENT_SCOPE)
@@ -14,8 +14,11 @@ endfunction()
 
 function( GetTypedLibrary result targetType libraryName )
 	GetLibraryType(name type ${libraryName})
-	if (type STREQUAL static)
+	if (type STREQUAL interface)
+		set(${result} ${name} PARENT_SCOPE)
+	elseif (type STREQUAL static)
 		set(${result} s_${name} PARENT_SCOPE)
+		message ("${libraryName} : ${targetType}")
 	elseif(type STREQUAL shared)
 		set(${result} ${name} PARENT_SCOPE)
 	elseif(type STREQUAL both)
@@ -29,6 +32,7 @@ endfunction( GetTypedLibrary )
 
 
 function( GetTypedLibraries result targetType )
+	set(${result} "" PARENT_SCOPE)
 	set(results "")
 	foreach(arg ${ARGN})
 		GetTypedLibrary(res ${targetType} ${arg})
