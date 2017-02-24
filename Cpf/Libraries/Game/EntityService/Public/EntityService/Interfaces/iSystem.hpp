09ad4b85d3cfc786e7b0af615483bc499e267f61
Kelly Brock <all8up@gmail.com>
2017-02-23 20:25:09 -0600

    Missed files.

---
+++ Cpf/Libraries/Game/EntityService/Public/EntityService/Interfaces/iSystem.hpp
@@ -0,0 +1,14 @@
+//////////////////////////////////////////////////////////////////////////
+#pragma once
+#include "RefCounted.hpp"
+
+namespace Cpf
+{
+	namespace EntityService
+	{
+		struct iSystem : iUnknown
+		{
+			
+		};
+	}
+}
