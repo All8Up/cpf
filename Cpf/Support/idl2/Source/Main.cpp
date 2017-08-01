cc6884fc406d3d277b3fa15822c5217e98b3e1bf
Kelly Brock <All8Up@gmail.com>
2017-07-31 20:45:48 -0500

    Trying to get the generated parser working.

---
+++ Cpf/Support/idl2/Source/Main.cpp
@@ -0,0 +1,65 @@
+#include "antlr4-runtime.h"
+#include "IDLLexer.h"
+#include "IDLParser.h"
+#include "IDLParserBaseListener.h"
+#include <Windows.h>
+
+#pragma execution_character_set("utf-8")
+
+class Listener : public IDLParserBaseListener
+{
+	void enterImport_stmt(IDLParser::Import_stmtContext * ctx) override
+	{
+		auto lit = ctx->STRING_LIT();
+
+		if (lit)
+		{
+
+		}
+	}
+	void exitImport_stmt(IDLParser::Import_stmtContext * ctx) override
+	{
+		auto lit = ctx->STRING_LIT();
+
+		if (lit)
+		{
+
+		}
+	}
+
+	void enterStruct_stmt(IDLParser::Struct_stmtContext * ctx) override
+	{
+		auto str_decl = ctx->struct_decl();
+		if (str_decl)
+		{
+			static volatile int a = 0;
+			++a;
+		}
+	}
+	void exitStruct_stmt(IDLParser::Struct_stmtContext * ctx) override
+	{
+		auto str_decl = ctx->struct_decl();
+		if (str_decl)
+		{
+			static volatile int a = 0;
+			++a;
+		}
+	}
+};
+
+int main(int argc, char** argv)
+{
+	antlr4::ANTLRFileStream input("C:/Projects/cpf/Cpf/Support/idl2/TestData/iRegistry.idl");
+	IDLLexer lexer(&input);
+	antlr4::CommonTokenStream tokenStream(&lexer);
+	IDLParser parser(&tokenStream);
+	Listener listener;
+	parser.addParseListener(&listener);
+
+	antlr4::tree::ParseTree* tree = parser.main();
+
+	std::wstring s = antlrcpp::s2ws(tree->toStringTree(&parser)) + L"\n";
+	OutputDebugString(s.data());
+
+	return 0;
+}
