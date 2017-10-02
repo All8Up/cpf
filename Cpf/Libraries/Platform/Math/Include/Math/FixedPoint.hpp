ec2f6d44482adfcce75ba44efe53574b437af6b7
Kelly Brock <All8Up@gmail.com>
2017-10-02 00:19:18 -0500

    Addded a basic start for fixed point math.

---
+++ Cpf/Libraries/Platform/Math/Include/Math/FixedPoint.hpp
@@ -0,0 +1,171 @@
+//////////////////////////////////////////////////////////////////////////
+#pragma once
+#include "Configuration.hpp"
+
+namespace CPF
+{
+	namespace Math
+	{
+		template <int TYPESIZE>
+		struct FPSize;
+		template <>
+		struct FPSize<32>
+		{
+			using ValueType = int32_t;
+			using FloatType = float;
+			using Intermediate = int64_t;
+		};
+		template <>
+		struct FPSize<64>
+		{
+			using ValueType = int64_t;
+			using FloatType = double;
+			using Intermediate = int64_t;
+		};
+
+		//////////////////////////////////////////////////////////////////////////
+		template <int TYPESIZE, int FRACBITS>
+		class FixedPoint
+		{
+		public:
+			using SizeType = FPSize<TYPESIZE>;
+			using ValueType = typename SizeType::ValueType;
+			using FloatType = typename SizeType::FloatType;
+			using Intermediate = typename SizeType::Intermediate;
+			static constexpr size_t kShift = FRACBITS;
+			static constexpr ValueType kMaxWhole = ValueType(-1) >> FRACBITS;
+			static constexpr ValueType kOneFrac = 1 << FRACBITS;
+			static constexpr ValueType kMaxFrac = (1 << FRACBITS) - 1;
+
+			FixedPoint(ValueType whole, ValueType fraction);
+			explicit FixedPoint(ValueType value);
+			explicit FixedPoint(float value);
+
+			explicit operator ValueType () const;
+			explicit operator FloatType () const;
+
+			FixedPoint<TYPESIZE, FRACBITS>& operator = (FixedPoint<TYPESIZE, FRACBITS> value);
+
+		private:
+			ValueType mData;
+		};
+
+
+		//////////////////////////////////////////////////////////////////////////
+		template <int TYPESIZE, int FRACBITS>
+		FixedPoint<TYPESIZE, FRACBITS>::FixedPoint(ValueType whole, ValueType fraction)
+			: mData((whole << kShift) | (fraction))
+		{
+			CPF_ASSERT((whole & ~kMaxWhole) == 0);
+			CPF_ASSERT(fraction <= kMaxFrac)
+		}
+
+		template <int TYPESIZE, int FRACBITS>
+		FixedPoint<TYPESIZE, FRACBITS>::FixedPoint(ValueType value)
+			: mData(value)
+		{}
+		template <int TYPESIZE, int FRACBITS>
+		FixedPoint<TYPESIZE, FRACBITS>::FixedPoint(float value)
+		{
+			float v = std::abs(value);
+			mData = (ValueType(v) << kShift) |
+				ValueType((v - ValueType(v)) * kOneFrac);
+			mData = std::signbit(value) ? -mData : mData;
+		}
+
+		template <int TYPESIZE, int FRACBITS>
+		FixedPoint<TYPESIZE, FRACBITS>::operator ValueType () const
+		{
+			return mData;
+		}
+
+		template <int TYPESIZE, int FRACBITS>
+		FixedPoint<TYPESIZE, FRACBITS>::operator FloatType () const
+		{
+			return FloatType(mData) / FloatType(kOneFrac);
+		}
+
+		template <int TYPESIZE, int FRACBITS>
+		FixedPoint<TYPESIZE, FRACBITS>& FixedPoint<TYPESIZE, FRACBITS>::operator = (FixedPoint<TYPESIZE, FRACBITS> value)
+		{
+			mData = value.mData;
+			return *this;
+		}
+
+		//////////////////////////////////////////////////////////////////////////
+		template <typename FPTYPE>
+		FPTYPE operator - (const FPTYPE value)
+		{
+			using ValueType = typename FPTYPE::ValueType;
+			return FPTYPE(-ValueType(value));
+		}
+
+		template <typename FPTYPE>
+		FPTYPE operator + (const FPTYPE lhs, const FPTYPE rhs)
+		{
+			using ValueType = typename FPTYPE::ValueType;
+			return FPTYPE(ValueType(lhs) + ValueType(rhs));
+		}
+
+		template <typename FPTYPE>
+		FPTYPE operator - (const FPTYPE lhs, const FPTYPE rhs)
+		{
+			using ValueType = typename FPTYPE::ValueType;
+			return FPTYPE(ValueType(lhs) - ValueType(rhs));
+		}
+
+		template <typename FPTYPE>
+		FPTYPE operator * (const FPTYPE lhs, const FPTYPE rhs)
+		{
+			using ValueType = typename FPTYPE::ValueType;
+			using Intermediate = typename FPTYPE::Intermediate;
+			Intermediate result = Intermediate(ValueType(lhs)) * Intermediate(ValueType(rhs));
+			return FPTYPE(ValueType(result >> FPTYPE::kShift));
+		}
+
+		template <typename FPTYPE>
+		FPTYPE operator / (const FPTYPE lhs, const FPTYPE rhs)
+		{
+			using ValueType = typename FPTYPE::ValueType;
+			using Intermediate = typename FPTYPE::Intermediate;
+			Intermediate result = (Intermediate(ValueType(lhs)) << FPTYPE::kShift) / Intermediate(ValueType(rhs));
+			return FPTYPE(ValueType(result));
+		}
+
+		//////////////////////////////////////////////////////////////////////////
+		template <typename FPTYPE>
+		FPTYPE operator == (const FPTYPE lhs, const FPTYPE rhs)
+		{
+			using ValueType = typename FPTYPE::ValueType;
+			return ValueType(lhs) == ValueType(rhs);
+		}
+
+		template <typename FPTYPE>
+		FPTYPE operator > (const FPTYPE lhs, const FPTYPE rhs)
+		{
+			using ValueType = typename FPTYPE::ValueType;
+			return ValueType(lhs) > ValueType(rhs);
+		}
+
+		template <typename FPTYPE>
+		FPTYPE operator >= (const FPTYPE lhs, const FPTYPE rhs)
+		{
+			using ValueType = typename FPTYPE::ValueType;
+			return ValueType(lhs) >= ValueType(rhs);
+		}
+
+		template <typename FPTYPE>
+		FPTYPE operator < (const FPTYPE lhs, const FPTYPE rhs)
+		{
+			using ValueType = typename FPTYPE::ValueType;
+			return ValueType(lhs) < ValueType(rhs);
+		}
+
+		template <typename FPTYPE>
+		FPTYPE operator <= (const FPTYPE lhs, const FPTYPE rhs)
+		{
+			using ValueType = typename FPTYPE::ValueType;
+			return ValueType(lhs) <= ValueType(rhs);
+		}
+	}
+}
