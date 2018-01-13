7cd1b13de92595ea020aac48a4344674593900f5
Kelly Brock <all8up@gmail.com>
2018-01-13 17:50:44 -0600

    Removed the resource ID, resource system is fully IDL.

--- Core/Include/CPF/Hash/Crc.hpp
+++ Core/Include/CPF/Hash/Crc.hpp
@@ -5,7 +5,7 @@
 #include "CPF/Hash/Detail/Crc16Table.hpp"
 #include "CPF/Hash/Detail/Crc32Table.hpp"
 #include "CPF/Hash/Detail/Crc64Table.hpp"
-
+#include "CPF/Std/String.hpp"
 
 namespace CPF
 {
@@ -121,8 +121,10 @@ namespace CPF
 		/**
 		 * @brief Compute a 32 bit crc from the given data.
 		 */
-		constexpr uint32_t Crc32(const char* const data, size_t length)
+		constexpr uint32_t Crc32(const char* const data, size_t length = 0)
 		{
+			if (length == 0)
+				length = Std::StrLen(data);
 			return ComputeCrc32(data, length, uint32_t(-1));
 		}
 
