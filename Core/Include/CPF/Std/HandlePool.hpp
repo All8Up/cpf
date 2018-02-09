29373706559afc4ca88de48f3f13859a04b87195
Kelly Brock <All8Up@gmail.com>
2018-02-08 22:26:26 -0600

    Added a handle based sparse vector. Starting to flesh out the behavior of the rendering engine.

--- Core/Include/CPF/Std/HandlePool.hpp
+++ Core/Include/CPF/Std/HandlePool.hpp
@@ -2,6 +2,7 @@
 #pragma once
 #include "CPF/Std/Vector.hpp"
 #include "CPF/Std/Move.hpp"
+#include "CPF/Std/Pair.hpp"
 
 namespace CPF
 {
@@ -54,6 +55,9 @@ namespace CPF
 		Handle PredictHandle(size_t futureCount);
 
 	private:
+		template <typename, typename, size_t>
+		friend class SparseHandleVector;
+
 		HandlePool(const HandlePool&) = delete;
 		HandlePool& operator = (const HandlePool&) = delete;
 
@@ -254,4 +258,35 @@ namespace CPF
 		mHandles[idx].mVersion = kInvalidIndex;
 		mFirstFree = uint32_t(idx);
 	}
+
+	//////////////////////////////////////////////////////////////////////////
+	template <typename DATA_TYPE, typename HANDLE_TYPE = uint64_t, size_t BLOCK_SIZE = 1024>
+	class SparseHandleVector
+	{
+	public:
+		HANDLE_TYPE Insert(DATA_TYPE&& data)
+		{
+			auto result = mHandles.Alloc(uint32_t(mData.size()));
+			mData.push_back({uint32_t(mData.size()), Move(data)});
+			return result;
+		}
+		void Erase(HANDLE_TYPE handle)
+		{
+			auto index = mHandles.Get(handle);
+			auto& data = mData[index];
+
+			if (index < mData.size() - 1)
+			{
+				data = mData.back();
+				mHandles.mHandles[mData.back().first].mData = index;
+			}
+			mData.pop_back();
+		}
+
+	private:
+		HandlePool<HANDLE_TYPE, BLOCK_SIZE> mHandles;
+		using DataPair = Pair<uint32_t, DATA_TYPE>;
+		using DataVector = Vector<DataPair>;
+		DataVector mData;
+	};
 }
