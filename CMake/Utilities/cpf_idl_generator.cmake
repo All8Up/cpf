64c3a9381eef9780103e5a69610d6b056a7be93d
Kelly Brock <all8up@gmail.com>
2018-03-19 09:44:58 -0500

    Weekend work.  Updated Graphics and Platform to use new IDL pattern. Moved Graphics and Platform examples so that the plugins are now fully self contained drop in directories.

--- CMake/Utilities/cpf_idl_generator.cmake
+++ CMake/Utilities/cpf_idl_generator.cmake
@@ -1,20 +1,25 @@
 function (cpf_idl_generator)
 	set (options
 		REQUIRED
-		MAKE_RELEASE
+		PACKAGE_INTERFACES
 	)
 	set (single_value
 		TARGET
 		BASE
 		OUTPUT_ROOT
 		OUTPUT_BASE
+		IDE_FOLDER
 		IDL_ROOT
+		HPP_ROOT
+		HPP_OUTPUT_BASE
 	)
 	set (multi_value
 		FILES
+		COPY_HPP
 	)
 	cmake_parse_arguments (IDL "${options}" "${single_value}" "${multi_value}" ${ARGN})
 
+	# Generated files.
 	set (generated_files "")
 	set (lang_target "cpp")
 	set (lang_dir "CPP")
@@ -41,25 +46,47 @@ function (cpf_idl_generator)
 		source_group (Include\\${sub_directory} FILES ${target_file})
 	endforeach ()
 
-	add_custom_target (${IDL_TARGET} SOURCES ${generated_files} ${IDL_FILES})
-	set_property (TARGET ${IDL_TARGET} PROPERTY FOLDER ${IDL_OUTPUT_BASE})
+	add_custom_target (${IDL_TARGET} SOURCES ${generated_files} ${IDL_COPY_HPP} ${IDL_FILES})
+	set_property (TARGET ${IDL_TARGET} PROPERTY FOLDER ${IDL_IDE_FOLDER})
 	add_dependencies (${IDL_BASE} ${IDL_TARGET})
 
-	if (IDL_MAKE_RELEASE)
+	# CPP header copies.
+	if (IDL_COPY_HPP)
+		foreach (filename ${IDL_COPY_HPP})
+			get_filename_component (directory ${filename} DIRECTORY)
+			file (RELATIVE_PATH relative ${IDL_HPP_ROOT} ${filename})
+			get_filename_component (sub_directory ${relative} DIRECTORY)
+			get_filename_component (fileName ${filename} NAME)
+
+			set (target_file "${IDL_OUTPUT_ROOT}/${lang_dir}/${IDL_HPP_OUTPUT_BASE}/${sub_directory}/${fileName}")
+
+			add_custom_command (
+				TARGET ${IDL_TARGET} POST_BUILD
+				COMMAND ${CMAKE_COMMAND} -E copy
+					${filename}
+					${target_file}
+			)
+
+			source_group (Include FILES ${filename})
+		endforeach ()
+	endif ()
+
+	# Create packaging utilities.
+	if (IDL_PACKAGE_INTERFACES)
 		set (archive_files "")
 		foreach (filename ${generated_files})
 			file (RELATIVE_PATH relative "${CMAKE_BINARY_DIR}/Generated" ${filename})
 			list (APPEND archive_files "${relative}")
 		endforeach ()
 
-		add_custom_target(MakeRelease_${IDL_TARGET}
+		add_custom_target(PackageInterfaces_${IDL_TARGET}
 			WORKING_DIRECTORY
 				"${CMAKE_BINARY_DIR}/Generated"
 			COMMAND
-				${CMAKE_COMMAND} -E make_directory "${CMAKE_BINARY_DIR}/Releases"
+				${CMAKE_COMMAND} -E make_directory "${CMAKE_BINARY_DIR}/Packages"
 			COMMAND
-			    ${CMAKE_COMMAND} -E tar "cfv" "${CMAKE_BINARY_DIR}/Releases/${lang_dir}_${IDL_TARGET}.zip" --format=zip ${archive_files}
+			    ${CMAKE_COMMAND} -E tar "cfv" "${CMAKE_BINARY_DIR}/Packages/${lang_dir}_${IDL_TARGET}.zip" --format=zip ${archive_files}
 		)
-		set_property (TARGET MakeRelease_${IDL_TARGET} PROPERTY FOLDER Releases)
+		set_property (TARGET PackageInterfaces_${IDL_TARGET} PROPERTY FOLDER Packages)
 	endif ()
 endfunction (cpf_idl_generator)
